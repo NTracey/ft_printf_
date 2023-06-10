e73dab9da76d2126a877b27e755742f8ded176c2
Tracey <traceyngo8@gmail.com>
2023-06-11 06:47:41 +0930

    refactored format_specifier.c:

--- format_specifier.c
+++ format_specifier.c
@@ -1,40 +1,60 @@
 #include "ft_printf.h"
-
 void handle_format_specifier(const char *format, va_list args)
 {
     if (*format == 'c')
-    {
-        int ch = va_arg(args, int);
-        ft_putchar((char)ch);
-    }
+        ft_putchar((char)va_arg(args, int));
     else if (*format == '%')
-    {
-        int ch = va_arg(args, int);
-        ft_percent((char)ch);
-    }
+        ft_percent((char)va_arg(args, int));
     else if (*format == 's')
-    {
-        char *str = va_arg(args, char *);
-        ft_putstr(str);
-    }
+        ft_putstr(va_arg(args, char *));
     else if (*format == 'd' || *format == 'i')
-    {
-        int num = va_arg(args, int);
-        ft_dec_int(num);
-    }
+        ft_dec_int(va_arg(args, int));
     else if (*format == 'u')
-    {
-        int num = va_arg(args, int);
-        ft_unsigned_decimal(num);
-    }
+        ft_unsigned_decimal(va_arg(args, int));
     else if (*format == 'x' || *format == 'X')
-    {
-        unsigned int num = va_arg(args, unsigned int);
-        ft_hex(num, *format == 'x');
-    }
+        ft_hex(va_arg(args, unsigned int), *format == 'x');
     else if (*format == 'p')
     {
-        void* num = va_arg(args, void*);
+        void *num = va_arg(args, void *);
         ft_pointer(num);
     }
 }
+
+// void handle_format_specifier(const char *format, va_list args)
+// {
+//     if (*format == 'c')
+//     {
+//         int ch = va_arg(args, int);
+//         ft_putchar((char)ch);
+//     }
+//     else if (*format == '%')
+//     {
+//         int ch = va_arg(args, int);
+//         ft_percent((char)ch);
+//     }
+//     else if (*format == 's')
+//     {
+//         char *str = va_arg(args, char *);
+//         ft_putstr(str);
+//     }
+//     else if (*format == 'd' || *format == 'i')
+//     {
+//         int num = va_arg(args, int);
+//         ft_dec_int(num);
+//     }
+//     else if (*format == 'u')
+//     {
+//         int num = va_arg(args, int);
+//         ft_unsigned_decimal(num);
+//     }
+//     else if (*format == 'x' || *format == 'X')
+//     {
+//         unsigned int num = va_arg(args, unsigned int);
+//         ft_hex(num, *format == 'x');
+//     }
+//     else if (*format == 'p')
+//     {
+//         void* num = va_arg(args, void*);
+//         ft_pointer(num);
+//     }
+// }
--- test.c
+++ test.c
@@ -66,19 +66,21 @@ void ft_pointer(const void *ptr)
 void handle_format_specifier(const char *format, va_list args)
 {
     if (*format == 'c')
-    {
-        int ch = va_arg(args, int);
-        ft_putchar((char)ch);
-    }
+        ft_putchar((char)va_arg(args, int));
+    // else if (*format == '%')
+    //     ft_percent((char)va_arg(args, int));
     else if (*format == 's')
-    {
-        char *str = va_arg(args, char *);
-        ft_putstr(str);
-    }
+        ft_putstr(va_arg(args, char *));
+    // else if (*format == 'd' || *format == 'i')
+    //     ft_dec_int(va_arg(args, int));
+    // else if (*format == 'u')
+    //     ft_unsigned_decimal(va_arg(args, int));
+    // else if (*format == 'x' || *format == 'X')
+        // ft_hex(va_arg(args, unsigned int), *format == 'x');
     else if (*format == 'p')
     {
-        void *ptr = va_arg(args, void *);
-        ft_pointer(ptr);
+        void *num = va_arg(args, void *);
+        ft_pointer(num);
     }
 }
 
@@ -115,7 +117,7 @@ int main()
     printf("\nReturn value: %d\n", c);
 
     int ch = ft_printf("ft_printf: %c, %c, %c, %c, %c", 'A', '9', '-', 48, '\0');
-    printf("\nReturn value: %d\n", (ch - 3));
+    printf("\nReturn value: %d\n", ch);
 
     printf("\nSTRING\n");
 
