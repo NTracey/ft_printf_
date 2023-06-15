ea0a44170508581114bb36316de509465cdc9557
Tracey <traceyngo8@gmail.com>
2023-06-16 02:42:06 +0930

    removed the helper functions that's not need in hex, and pointer file

--- ft_hex.c
+++ ft_hex.c
@@ -1,33 +1,5 @@
 #include "ft_printf.h"
-// #include <unistd.h>
 // long is needed for %p since the number is too big to print out
-
-// Function to convert a decimal number to its hexadecimal ASCII representation
-// int convert_to_ascii(unsigned long int decimal_number, int lowercase)
-// {
-//     int i;
-//     int remainder;
-//     char hexa_num[100];
-
-//     i = 0;
-//     if (decimal_number == 0)
-//         hexa_num[i++] = '0';
-//     else // handles positive decimal numbers
-//     {
-//         while (decimal_number != 0)
-//         {
-//             remainder = decimal_number % 16;
-//             if (remainder < 10)
-//                 hexa_num[i++] = remainder + '0'; // Convert to ASCII character
-//             else
-//                 hexa_num[i++] = remainder + (lowercase ? 'a' : 'A') - 10;
-//             decimal_number = decimal_number / 16;
-//         }
-//     }
-//     while (--i >= 0) // Print the hex number in reverse order
-//         ft_putchar(hexa_num[i]);
-//     return 0;
-// }
 
 void ft_hex(unsigned long int decimal_number, int lowercase)
 {
